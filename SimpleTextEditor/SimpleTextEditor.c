a7077799d7fc6369c9b0ec86373e313a74ed186c
Annnnnaaaaaaa <dnipro.anna@gmail.com>
2026-06-09 23:38:03 +0300

    added appendText function

--- SimpleTextEditor/SimpleTextEditor.c
+++ SimpleTextEditor/SimpleTextEditor.c
@@ -1,59 +1,71 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <string.h>
-
-
 
 int main()
 {
     int command;
-    scanf(" %d", &command);
+
+    // Перша безпечна перевірка вводу на старті програми
+    if (scanf("%d", &command) != 1)
+    {
+        printf("Invalid input! Please enter a number.\n");
+        while (getchar() != '\n');
+        command = -1; // Присвоюємо значення, щоб увійти в цикл і попросити ввід знову
+    }
+    else
+    {
+        while (getchar() != '\n');
+    }
 
     while (command != 0)
     {
         switch (command)
         {
         case 1:
-            printf("Command 1 is not implemented yet");
+            printf("Command 1 is not implemented yet\n");
             break;
 
         case 2:
-            printf("Command 2 is not implemented yet");
+            printf("Command 2 is not implemented yet\n");
             break;
 
         case 3:
-            printf("Command 3 is not implemented yet");
+            printf("Command 3 is not implemented yet\n");
             break;
 
         case 4:
-            printf("Command 4 is not implemented yet");
+            printf("Command 4 is not implemented yet\n");
             break;
 
         case 5:
-            printf("Command 5 is not implemented yet");
+            printf("Command 5 is not implemented yet\n");
             break;
 
         case 6:
-            printf("Command 6 is not implemented yet");
+            printf("Command 6 is not implemented yet\n");
             break;
 
         case 7:
-            printf("Command 7 is not implemented yet");
+            printf("Command 7 is not implemented yet\n");
             break;
 
         default:
-            printf("This command does not exist");
-            scanf(" %d", &command);
+            printf("This command does not exist\n");
             break;
         }
 
+        // Безпечний ввід у кінці циклу — захищає від літер та очищає буфер
         if (scanf("%d", &command) != 1)
         {
             printf("Invalid input! Please enter a number.\n");
-            while (getchar() != '\n'); // Викидаємо літеру 'а' з буфера, щоб не зациклювалось
+            while (getchar() != '\n');
         }
         else
         {
-            while (getchar() != '\n'); // Викидаємо Enter (\n) після успішного числа
+            while (getchar() != '\n');
         }
     }
+
+    return 0;
 }
